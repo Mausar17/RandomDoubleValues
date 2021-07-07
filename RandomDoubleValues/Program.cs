ae078085ddb9c451f9755a8a6979d24e9cdd7913
mausar17 <mausar_17@hotmail.com>
2021-07-06 23:29:18 -0500

    Add project files.

---
+++ RandomDoubleValues/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RandomDoubleValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            double[] randomDoubles = new double[20];
+
+            for (int i = 0; i < randomDoubles.Length; i++)
+            {
+                randomDoubles[i] = random.NextDouble();
+            }
+
+            foreach (var value in randomDoubles)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
